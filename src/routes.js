3d0a83a1a8269ccdb0b7e8653b53baeaa9410811
Craig Melville <acekreations@gmail.com>
2020-10-02 22:26:44 -0700

    reset

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import HomePage from "./components/HomePage";
-import ProjectPage from "./components/ProjectPage";
-
-const routes = [
-    { path: "/", component: HomePage },
-    { path: "/project/:id", component: ProjectPage }
-];
-
-export default routes;
